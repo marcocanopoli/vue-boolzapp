4f250fa88b4b0acb0656349061f48f151fb426f0
marcocanopoli <canopoli.marco@gmail.com>
2021-05-21 14:17:07 +0200

    Milestone 2

--- js/script.js
+++ js/script.js
@@ -22,6 +22,11 @@ var app = new Vue({
                         date: '10/01/2020 16:15:22',
                         text: 'Tutto fatto!',
                         status: 'received'
+                    },
+                    {
+                        date: '20/05/2021 16:15:22',
+                        text: 'Test ieri :Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis, illo?',
+                        status: 'received'
                     }
                 ],
             },
@@ -43,6 +48,11 @@ var app = new Vue({
                     {
                         date: '20/03/2020 16:35:00',
                         text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'sent'
+                    },
+                    {
+                        date: '21/05/2021 16:35:00',
+                        text: 'Test oggi :Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis, illo?',
                         status: 'sent'
                     }
                 ],
@@ -90,12 +100,60 @@ var app = new Vue({
         activeChatIndex: 0
     },
     methods: {
+        getNowDateTime: function () {
+            return dayjs().format('DD/MM/YYYY HH:mm:ss');
+        },
+        getMsgTime: function(message) {
+            const msgTime = message.date.split(" ")[1];
+            return msgTime.substr(0, 5);
+        },
         getContactImg: function(contact) {
             return `img/avatar${contact.avatar}.jpg`;
         },
         getLastMsg: function(contact) {
             return contact.messages[contact.messages.length - 1];
-        }
-    }
+        },
+        getLastMsgDate: function(contact) {
+            const todayDateTime = this.getNowDateTime();
+            const todayDate = todayDateTime.split(" ")[0];
+            const todayDay = todayDateTime.substr(0, 2);          
+            const todayMonthYear = todayDateTime.substr(2, 8);         
+            const msgDateTime =  this.getLastMsg(contact).date;
+            const msgDate = msgDateTime.split(" ")[0];
+            const msgDay = msgDateTime.substr(0, 2);
+            const msgMonthYear = msgDateTime.substr(2, 8);
+            const msgHoursMins = msgDateTime.substr(10, 6);
+            console.log(todayDay, msgDay);
+            let lastMsgTime;
 
+            if (msgDate == todayDate) {
+                lastMsgTime = `oggi ${msgHoursMins}`
+            } else if (msgMonthYear == todayMonthYear && msgDay == todayDay - 1) {
+                lastMsgTime = `ieri ${msgHoursMins}`
+            } else {
+                lastMsgTime = `${msgDate} ${msgHoursMins}`
+            }
+            return lastMsgTime;
+        },        
+        getLastMsgText: function(contact) {
+            return this.getLastMsg(contact).text;
+        },
+        text40: function(text) {
+            let newText = text;
+            if (text.length > 35) {
+                newText = `${text.substring(0, 40)}...`;
+            }
+            return newText;
+        },
+        setActive: function(chatIndex) {
+            console.log(this.activeChatIndex);
+            this.activeChatIndex = chatIndex;
+        },
+        getActiveContact: function() {
+            return this.contacts[this.activeChatIndex];
+        },
+        getActiveImg: function() {
+            return `img/avatar${this.contacts[this.activeChatIndex].avatar}.jpg`;
+        },
+    }
 });
